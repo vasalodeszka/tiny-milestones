2975e32c10d7bc206e7a10f83cd12460020659c3
vasalodeszka <janosmatula@yahoo.com>
2024-11-19 17:57:40 +0100

    chore: create users app with a User model

---
+++ apps/users/models.py
@@ -0,0 +1,46 @@
+import uuid
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+from django.db import models, transaction
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    @transaction.atomic
+    def _create_user(self, username, password, **extra_fields):
+        username = username
+        fields = {
+            **extra_fields,
+        }
+        user = self.model(**fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")  # noqa: EM101
+
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")  # noqa: EM101
+
+        return self._create_user(username, password, **extra_fields)
+
+
+class User(AbstractUser, PermissionsMixin):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    USERNAME_FIELD = "username"
