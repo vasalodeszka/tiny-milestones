1ca5b16a3ecaf5044be4ce7d508d2b710eca57af
vasalodeszka <janosmatula@yahoo.com>
2024-12-03 10:34:26 +0100

    feat: configure django-debug-toolbar in local env

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,7 +2,7 @@
 from datetime import timedelta
 
 from .base import *  # noqa: F403
-from .base import env
+from .base import ALLOWED_HOSTS, INSTALLED_APPS, MIDDLEWARE, SIMPLE_JWT, env
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
@@ -14,8 +14,32 @@ SECRET_KEY = env(
 )
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", ".localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+ALLOWED_HOSTS += ["localhost", ".localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 
 ADMIN_URL = "admin/"
 
-SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"] = timedelta(hours=24)  # noqa: F405
+SIMPLE_JWT["ACCESS_TOKEN_LIFETIME"] = timedelta(hours=24)
+
+# django-debug-toolbar
+# ------------------------------------------------------------------------------
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
+INSTALLED_APPS += ["debug_toolbar"]
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+DEBUG_TOOLBAR_CONFIG = {
+    "DISABLE_PANELS": [
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        # Disable profiling panel due to an issue with Python 3.12:
+        # https://github.com/jazzband/django-debug-toolbar/issues/1875
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+    ],
+    "SHOW_TEMPLATE_CONTEXT": True,
+}
+# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
+INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
+if env("USE_DOCKER") == "yes":
+    import socket
+
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS += [".".join(ip.split(".")[:-1] + ["1"]) for ip in ips]
